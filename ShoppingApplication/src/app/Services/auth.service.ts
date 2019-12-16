6c4ff681861b81be8b10a56b4f2804875f645f48
sej <sej@ciklum.com>
2019-12-16 18:34:54 +0500

    Added Event Emitter

--- ShoppingApplication/src/app/Services/auth.service.ts
+++ ShoppingApplication/src/app/Services/auth.service.ts
@@ -5,7 +5,7 @@ import {map} from 'rxjs/operators';
   providedIn: 'root'
 })
 export class AuthService {
-  baseUrl = 'https://localhost:44354/api/auth/'
+  baseUrl = 'https://localhost:44354/api/auth/';
 
 constructor(private http: HttpClient) { }
 login(model: any) {
