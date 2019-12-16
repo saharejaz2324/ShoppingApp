0e5a8c2956170740cd189c48e123bd478d0bac52
sej <sej@ciklum.com>
2019-12-16 18:34:11 +0500

    Parent to Child Component values done

--- ShoppingApplication/src/app/app.module.ts
+++ ShoppingApplication/src/app/app.module.ts
@@ -4,19 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {HttpClientModule} from '@angular/common/http';
-import { ValueComponent } from './Components/Value/Value.component';
+import { NavComponent } from './Components/Value/nav/nav.component';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from './Services/auth.service';
+import { HomeComponent } from './Components/Value/home/home.component';
+import { RegisterComponent } from './Components/Value/register/register.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValueComponent
+    NavComponent,
+    HomeComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-   HttpClientModule
+   HttpClientModule,
+   FormsModule
   ],
-  providers: [],
+  providers: [
+    AuthService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
