359d9a7ba3aebfb7eaf741b9d313d5e822446c7a
sej <sej@ciklum.com>
2019-12-19 13:07:08 +0500

    Fix the photo for a newly create person

--- ShoppingApplication/src/app/Components/Value/register/register.component.ts
+++ ShoppingApplication/src/app/Components/Value/register/register.component.ts
@@ -3,6 +3,9 @@ import { AuthService } from 'src/app/Services/auth.service';
 import { AlterifyService } from 'src/app/Services/Alterify.service';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 import { BsDatepickerConfig } from 'ngx-bootstrap';
+import { User } from 'src/app/Models/User';
+import { Router } from '@angular/router';
+
 
 
 @Component({
@@ -13,14 +16,15 @@ import { BsDatepickerConfig } from 'ngx-bootstrap';
 export class RegisterComponent implements OnInit {
 //  @Input() valuesFromHome: any;
  @Output() cancelRegister = new EventEmitter();
-  model: any = {};
+ user: User;
   registerForm: FormGroup;
   bsConfig: Partial<BsDatepickerConfig>;
 
 
   constructor(private authService: AuthService,
               private alertify: AlterifyService,
-              private fb: FormBuilder
+              private fb: FormBuilder,
+              private router: Router
               ) { }
 
   ngOnInit() {
@@ -48,9 +52,22 @@ export class RegisterComponent implements OnInit {
   }
 
   register() {
-    console.log(this.registerForm.value);
-
-
+    // we are using reactive forms so we can also check in .ts file
+    if (this.registerForm.valid) {
+      this.user = Object.assign({}, this.registerForm.value);
+      // clone the value of register form into the empty object and assign that object to the user object
+      this.authService.register(this.user)
+      .subscribe(() => {
+        this.alertify.success('Registration Successful');
+      }, error => {
+        this.alertify.error(error);
+      }, () => {
+        this.authService.login(this.user)
+        .subscribe(() => {
+          this.router.navigate(['/members']);
+        });
+      });
+    }
     // this.authService.register(this.model).subscribe(data => {
     //  this.alertify.success('Registration successful');
     // }, error => {
